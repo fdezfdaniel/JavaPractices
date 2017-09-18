015f9e1145ac8b1ff00efbc7ff47276f0fe53e0f
dfernandez <dfernandez@tgc.mx>
2017-09-18 09:54:27 -0600

    StrategyPatten

---
+++ design_patterns/Strategy/FlyRocketPowerd.java
@@ -0,0 +1,5 @@
+public class FlyRocketPowerd implements FlyBehavior{
+	public void fly(){
+		System.out.println("I'm flying like a rocket!!!");
+	}
+}
---
+++ design_patterns/Strategy/MallarDuck.java
@@ -0,0 +1,10 @@
+public class MallarDuck extends Duck {
+	public MallarDuck(){
+		quackBehavior = new Quack();
+		flyBehavior = new FlyWithWings();
+	}
+
+	public void display(){
+		System.out.println("I'm a real MallarDuck");
+	}
+}
